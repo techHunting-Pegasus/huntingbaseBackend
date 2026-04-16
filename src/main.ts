6849c0582edd013ba8c9745b7606c6a6f76d2e1d
Ishpreet singh <ishpreetsingh8386@gmail.com>
2026-04-16 22:54:40 +0530

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
 app.enableCors({
-  origin: 'http://localhost:3000',
+  origin: true,
+  credentials: true,
 });
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
